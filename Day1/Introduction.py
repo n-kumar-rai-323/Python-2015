1632e138cbbcd29dd31ab958493ab1007ecdfa56
Nishan kumar Rai <infonkumarrai323@gmail.com>
2025-01-28 19:30:06 +0300

    Day1

---
+++ Day1/Introduction.py
@@ -0,0 +1,50 @@
+# Arithmetic Operator
+#Additon 
+a = 1
+b = 2
+
+print(a + b )  # Here  + is an addition operator 
+
+
+
+# Subtraction 
+print(a - b) 
+
+#Multipliaction
+print(a * b)
+
+#Division
+print(a / b)
+
+# Exponent
+print(a ** 2)
+
+print(a % 2) # 
+
+print( a // 2) # hare is a Floor Division
+
+#Relational Operators 
+# ==
+# >= 
+# <=
+# != 
+# >
+# <
+
+# Logical Operator
+# And  F T = F
+# Or  F T = T
+# Not T = F 
+
+# Identiy Operators : is and is not the identiy check operators
+#  is 
+print(a is b)
+# is not
+
+
+# Membership Operator 
+ages = [21,22,23,24]
+print(21 in ages)
+print(22 not in ages)
+name = "Nishan"
+print("a" in name)
