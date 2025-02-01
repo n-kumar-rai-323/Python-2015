20a7d655394aea289c0cf5e55d9c76b3350c27d1
Nishan kumar Rai <infonkumarrai323@gmail.com>
2025-02-01 08:15:46 +0300

    Day-4

---
+++ Day4/string.py
@@ -0,0 +1,7 @@
+# String in python are of immutable type 
+example = " "
+example = str() 
+
+# Escape Sequences 
+example = 'I\'m learning python'
+print(example)
---
+++ Day4/string_operation.py
@@ -0,0 +1,7 @@
+a = "world"
+b = "hello"
+c = a + b
+print(c)
+print(len(a))
+
+print(a[slice(1, 4)])
